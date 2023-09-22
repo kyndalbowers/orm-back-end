5c31431b5c714a35a47ae16c88ef39c68294fe0f
kyndalbowers <131105491+kyndalbowers@users.noreply.github.com>
2023-09-21 21:14:20 -0700

    mysql, node.js and .env set up

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
